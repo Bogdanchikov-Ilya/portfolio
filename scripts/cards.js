31fc2837bdbadd2f291a2b95923cb24aad67ebfa
Илья Богданчиков <test200373@gmail.com>
2022-02-19 01:33:41 +0300

    add: animation on info-block & animtion on card & fix: links url

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,34 +1,45 @@
 const data = [
   {
-    title: 'Layout',
-    descr: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
-    gitHub: 'https://github.com/',
+    title: 'Лендинг строительной компании',
+    description: 'Простой лендинг сайта стриотленой компании.' +
+      'Есть адаптив для планшетов и мобильных устройств. ' +
+      'Использовал scss, разбил секции по scss компонентам. ' +
+      'Использовал слайдер Swiper Slider',
+    skills: 'SCSS, Swiper Slider',
+    gitHub: 'https://github.com/Bogdanchikov-Ilya/railway-construction',
+    link: 'https://bogdanchikov-ilya.github.io/railway-construction',
     image: '../../assets/img/sliders/33333.png'
   },
   {
     title: 'Vue + Laravel',
-    descr: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    description: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
     gitHub: 'https://github.com/',
     image: '../../assets/img/sliders/33333.png'
   },
   {
     title: 'Admin - panel',
-    descr: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    description: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
     gitHub: 'https://github.com/',
     image: '../../assets/img/sliders/33333.png'
   },
   {
     title: 'Shop on Nuxt js',
-    descr: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
+    description: 'lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem',
     gitHub: 'https://github.com/',
     image: '../../assets/img/sliders/33333.png'
   },
 ]
+
+function addCloseEvent () {
+  document.querySelector('.close-btn').addEventListener('click', () => {
+    document.querySelector('.cards-container').classList.remove('container-open')
+    document.querySelector('.info-block').remove()
+  })
+}
 export function loadCard () {
   data.forEach((item, index) => {
-    console.log(index)
     let htmlElem = `<div class="card"><span>${item.title}</span></div>`
-    document.querySelector('.cards-container').insertAdjacentHTML('afterbegin', htmlElem)
+    document.querySelector('.cards-container').insertAdjacentHTML('beforeend', htmlElem)
   })
 }
 
@@ -38,7 +49,23 @@ export function openCard() {
       // block
       let infoBlock =
       `<div class="info-block">
-        <h1>${data[index].title}</h1>
+        <div class="close-btn">
+          <span></span>
+          <span></span>
+        </div>
+        <div class="info-content-wrapper">
+          <h2 class="info-title">${data[index].title}</h2>
+          <p class="info-description">${data[index].description}</p>
+          <span class="info-skills">${data[index].skills}</span>
+          <div class="info-media">
+            <div class="link-wrapper">
+              <a href="https://github.com/Bogdanchikov-Ilya" id="github" target="_blank">Просмотреть</a>
+            </div>
+            <div class="link-wrapper">
+              <a href="https://github.com/Bogdanchikov-Ilya" id="github" target="_blank">Проект на GitHub</a>
+            </div>
+          </div>
+        </div>
       </div>`;
       // ---------------------
       document.querySelector('.cards-container').classList.add('container-open')
@@ -46,5 +73,8 @@ export function openCard() {
       if(block)
         block.remove()
       document.querySelector('.cards-wrapper').insertAdjacentHTML('afterbegin', infoBlock)
+
+      // delete button function
+      addCloseEvent()
     }))
 }
