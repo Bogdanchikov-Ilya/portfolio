e08d97af7966a9319651c9203f853ade193d12e0
Илья Богданчиков <test200373@gmail.com>
2022-02-20 00:36:20 +0300

    add: custom-slider

--- scripts/main-slider.js
+++ scripts/main-slider.js
@@ -1,2 +1,42 @@
+import { htmlCollection, selectedCardIndex, openCard } from "./cards.js";
+
 const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
+
+function setDisabledNavigation(){
+  (selectedCardIndex <= 0)
+  ? prevBtn.disabled = true
+  :prevBtn.disabled = false;
+
+  (selectedCardIndex >= htmlCollection.length - 1)
+  ? nextBtn.disabled = true
+  :nextBtn.disabled = false;
+}
+
+prevBtn.addEventListener('click', () => {
+  const prevIndex = selectedCardIndex - 1
+  openCard(htmlCollection[prevIndex], prevIndex)
+})
+
+nextBtn.addEventListener('click', () => {
+  const nextIndex = selectedCardIndex + 1
+  openCard(htmlCollection[nextIndex], nextIndex)
+})
+
+function renderPagination() {
+  console.log(htmlCollection.length)
+  const paginationContainer = document.querySelector('.slider-pagination')
+  paginationContainer.innerHTML = ''
+  for (let i = 0; i < htmlCollection.length; i++) {
+    (i === selectedCardIndex)
+    ? paginationContainer.insertAdjacentHTML('beforeend', '<div class="active"></div>')
+    : paginationContainer.insertAdjacentHTML('beforeend', '<div></div>')
+
+  }
+}
+
+function scrollToActive() {
+  console.log(document.querySelector('.container-open').scrollHeight)
+}
+
+export { setDisabledNavigation, renderPagination, scrollToActive }
