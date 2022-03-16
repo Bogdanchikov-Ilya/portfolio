05fe23d958f8fc0595cc085f46357620fdf99b47
Илья Богданчиков <test200373@gmail.com>
2022-03-17 00:57:21 +0300

    add: mobile cards

--- scripts/mobile-cards.js
+++ scripts/mobile-cards.js
@@ -1,3 +1,36 @@
-function renderMobileSlides() {
-  
+import { data } from './cards.js'
+
+let cardCollection;
+const cardContainer = document.querySelector('.cards-wrapper')
+const mediaBlock = document.querySelector('.media')
+
+// добавляю блок с ссылками
+document.querySelector('header').getElementsByClassName('container')[0].appendChild(mediaBlock)
+
+// удаляю лишние обертки
+while (cardContainer.firstChild){
+  cardContainer.firstChild.remove()
 }
+
+// добавляю контейнер
+let defaultContainer = document.createElement("div")
+defaultContainer.className = 'container'
+cardContainer.appendChild(defaultContainer)
+
+data.forEach((item) => {
+  let htmlElem = `<div class="card"><div class="front"><span>${item.title}</span></div><div class="back"><a href="${item.link}" target="_blank">Перейти</a><a href="${item.github}" target="_blank"><img src="https://img.icons8.com/ios-filled/30/ffffff/github-2.png"/>GitHub</a><div></div>`
+  cardContainer.getElementsByClassName('container')[0].insertAdjacentHTML('beforeend', htmlElem)
+})
+cardCollection = document.querySelectorAll('.card')
+
+cardCollection.forEach(item => {
+  item.addEventListener('click', () => {
+    // удаляю активные классы
+    cardCollection.forEach((item) => {
+      if(item.classList.contains('active')){
+        item.classList.remove('active')
+      }
+    })
+    item.classList.add('active')
+  })
+})
--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ import { typing } from './typing-effect.js'
 import { glitch } from './glitch.js';
 import { addListenerClickOpenCard, loadCard, openCard } from './cards.js';
 import { renderPagination } from './main-slider.js'
+
 window.onload = function() {
   // подгражю эффект печати
   typing('krike.xyz', document.querySelector('h1'), 250)
@@ -11,4 +12,12 @@ window.onload = function() {
   addListenerClickOpenCard(document.querySelectorAll('#open-card-link'))
   // scrollOnKeyDown()
   renderPagination()
+
+  if(window.innerWidth < 768){
+    let head = document.getElementsByTagName('head')[0];
+    let script = document.createElement("script")
+    script.src = "scripts/mobile-cards.js"
+    script.type = "module"
+    head.appendChild(script)
+  }
 }
